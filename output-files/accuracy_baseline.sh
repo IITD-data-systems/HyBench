61f1682b0ca1a7cb55b300c761599ff98f5f424c
Ayush29051 <122905924+Ayush29051@users.noreply.github.com>
2025-08-08 00:35:48 +0530

    Create accuracy_baseline.sh

---
+++ output-files/accuracy_baseline.sh
@@ -0,0 +1,4 @@
+bash accuracy_calculator.sh baseline hnswlib l2
+bash accuracy_calculator.sh baseline hnswlib cos
+bash accuracy_calculator.sh baseline ivfflat l2
+bash accuracy_calculator.sh baseline ivfflat cos
