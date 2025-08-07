a0e6a802a51a446d6d28c8928d5776fba37b2928
Ayush29051 <122905924+Ayush29051@users.noreply.github.com>
2025-08-08 00:41:54 +0530

    Update accuracy_baseline.sh

--- output-files/accuracy_baseline.sh
+++ output-files/accuracy_baseline.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Run accuracy calculation for the baseline implementation using hnswlib index with L2 distance
 bash accuracy_calculator.sh baseline hnswlib l2
+
+# Run accuracy calculation for the baseline implementation using hnswlib index with Cosine distance
 bash accuracy_calculator.sh baseline hnswlib cos
+
+# Run accuracy calculation for the baseline implementation using ivfflat index with L2 distance
 bash accuracy_calculator.sh baseline ivfflat l2
+
+# Run accuracy calculation for the baseline implementation using ivfflat index with Cosine distance
 bash accuracy_calculator.sh baseline ivfflat cos
